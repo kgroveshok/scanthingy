a9498a26160b59449f4c629a910239524ebbcb63
scanraspi <scanraspi@ksoft-creative-projects.co.uk>
2016-10-07 15:30:49 +0000

    tesseract tweaks

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+    
+sudo apt-get install sane libtiff* libjpeg-dev vim mc mysql-server scanbuttond git php5 apache2 libapache2-mod-php5
+
+cd /home/pi
+git clone git@github.com:kgroveshok/scanthingy.git
+
+
+cd /home/pi/scanthingy
+
+# wget http://www.leptonica.com/source/leptonica-1.73.tar.gz
+#     tar zxvf leptonica-1.73.tar.gz 
+#     cd leptonica-1.73/
+#     ./configure
+#    git clone https://github.com/tesseract-ocr/tesseract.git
+#    wget https://github.com/tesseract-ocr/tessdata/archive/master.zip
+
+cd /home/pi/scanthingy
+
+sh getopenkm.sh 
+
+cd /home/pi
+unzip owncloud-9.1.1.zip 
+
+#  125  ls
+#  126  mv owncloud-9.1.1.zip ..
+#  127  cd ..
+#  128  ls
+#  146  ls
+#  147  cd /var/
+#  148  ls
+#  149  cd www
+#  150  ls
+#  151  cd html/
+#  152  ls
+#  153  cd ..
+#  154  mv /home/pi/owncloud .
+#  155  sudo mv /home/pi/owncloud .
+#  156  ls -la
+#  157  sudo bash
+#  158  cd
+#  159  ls
--- scan.sh
+++ scan.sh
@@ -3,18 +3,45 @@
 
 # file name
 
-f=`date +%s`
+#f=`date +%s`
+f=`date +%Y%M%d-%H%m%S`
 
-#echo $f
+echo $f
 
 
 
-scanimage -x 215 -y 297 --format tiff --resolution 180 --mode Color >/home/pi/scan-$f.tiff
 
-tiff2pdf /home/pi/scan-$f.tiff -o /home/pi/scan-$f.pdf
+echo "Scanning"
 
-sudo mv /home/pi/scan-* /var/www/data/admin/files
 
+scanimage -p -x 215 -y 297 --format tiff --resolution 250 --mode Color >/tmp/scan-$f.tiff
+
+
+cd /tmp
+
+echo "Tiff to PDF"
+tiff2pdf scan-$f.tiff -o scan-$f.pdf
+
+echo "Tesseract"
+
+tesseract scan-$f.tiff -c load_system_dawg=false -c load_freq_dawg=false   -psm 1 -l eng  scan-text1-$f.txt 
+tesseract scan-$f.tiff  -c load_system_dawg=false -c load_freq_dawg=false  -psm 2 -l eng  scan-text2-$f.txt 
+tesseract scan-$f.tiff  -c load_system_dawg=false -c load_freq_dawg=false   -psm 3 -l eng  scan-text3-$f.txt 
+tesseract scan-$f.tiff  -c load_system_dawg=false -c load_freq_dawg=false   -psm 4 -l eng  scan-text4-$f.txt 
+# crap tesseract scan-$f.tiff -psm 5 -l eng  scan-text5-$f.txt 
+tesseract scan-$f.tiff  -c load_system_dawg=false -c load_freq_dawg=false   -psm 6 -l eng  scan-text6-$f.txt 
+# crap tesseract scan-$f.tiff -psm 7 -l eng  scan-text7-$f.txt 
+
+
+
+sudo chown www-data:www-data /tmp/scan*
+sudo chmod a=rwx /tmp/scan*
+
+# relocate files
+
+sudo mv /tmp/scan-* /var/www/data/admin/files -v
+
+# fix up perms
 
 
 # refresh owncloud
