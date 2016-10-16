b464163f857de5d70a0e2c62de59b41ce75a9a65
Kev On Laptop <kgroves@houseofkiya.co.uk>
2016-10-16 12:47:47 +0100

    change main menu size

--- desktop_client.sh
+++ desktop_client.sh
@@ -193,7 +193,7 @@ function packageall {
 # main menu
 
 while [[ 1 ]] ; do
-	dialog --menu "Main Menu" 15 25 5 "S" "Scanner" "C" "Copier (TODO)" "O" "OCR All New Scans" "F" "Re-OCR All Scans (TODO)" "P" "Repackage all PDFs" 2>/tmp/scanmenu
+	dialog --menu "Main Menu" 15 35 5 "S" "Scanner" "C" "Copier (TODO)" "O" "OCR All New Scans" "F" "Re-OCR All Scans (TODO)" "P" "Repackage all PDFs" 2>/tmp/scanmenu
 
 	if [[ $? -ne 0 ]] ; then
 		exit
