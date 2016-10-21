01a17a70c14584d2f9b25662dd122a6b28916d48
Kev On Laptop <kgroves@houseofkiya.co.uk>
2016-10-21 12:15:10 +0100

    bits

--- install.sh
+++ install.sh
@@ -54,21 +54,3 @@ cd
 #cd /home/pi
 #unzip owncloud-9.1.1.zip 
 
-#  125  ls
-#  126  mv owncloud-9.1.1.zip ..
-#  127  cd ..
-#  128  ls
-#  146  ls
-#  147  cd /var/
-#  148  ls
-#  149  cd www
-#  150  ls
-#  151  cd html/
-#  152  ls
-#  153  cd ..
-#  154  mv /home/pi/owncloud .
-#  155  sudo mv /home/pi/owncloud .
-#  156  ls -la
-#  157  sudo bash
-#  158  cd
-#  159  ls
